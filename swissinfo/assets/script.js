22dff191aeaf162bb3027d3f0cb5e0bf25ccdc22
Marjun Ravanes <jun2mar@gmail.com>
2024-10-18 17:35:35 +0800

    Update

--- swissinfo/assets/script.js
+++ swissinfo/assets/script.js
@@ -16,17 +16,17 @@ var advanced_ads_sticky_settings = { "check_position_fixed": "", "sticky_class":
 // var advanced_ads_cookies = { "cookie_path": "\/", "cookie_domain": "" };
 var advadsCfpInfo = { "cfpExpHours": "3", "cfpClickLimit": "3", "cfpBan": "7", "cfpPath": "\/eng", "cfpDomain": "#" };
 
-var winners = ["John D. won $500", "Sarah M. won $1,000", "Mike T. won $750", "Emma L. won $2,000"];
-setInterval(function () {
-    var winnerFeed = document.getElementById("winner-feed");
-    var newWinner = winners[Math.floor(Math.random() * winners.length)];
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(newWinner));
-    winnerFeed.insertBefore(li, winnerFeed.firstChild);
-    if (winnerFeed.children.length > 5) {
-        winnerFeed.removeChild(winnerFeed.lastChild);
-    }
-}, 5000); // Add a new winner every 5 seconds
+// var winners = ["John D. won $500", "Sarah M. won $1,000", "Mike T. won $750", "Emma L. won $2,000"];
+// setInterval(function () {
+//     var winnerFeed = document.getElementById("winner-feed");
+//     var newWinner = winners[Math.floor(Math.random() * winners.length)];
+//     var li = document.createElement("li");
+//     li.appendChild(document.createTextNode(newWinner));
+//     winnerFeed.insertBefore(li, winnerFeed.firstChild);
+//     if (winnerFeed.children.length > 5) {
+//         winnerFeed.removeChild(winnerFeed.lastChild);
+//     }
+// }, 5000); // Add a new winner every 5 seconds
 
 
 document.addEventListener('DOMContentLoaded', () => {
